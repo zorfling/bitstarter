720de926ed419a7f5afb3981e5f091024f356a3d
Chris Colborne <zorfling@gmail.com>
2013-07-07 07:33:30 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var data = fs.readFileSync('index.html');
+  var buffer = new Buffer(data);
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
